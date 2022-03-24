881e37e85a2f74b49c4c533b61688efba3054ba9
Tom Sitter <thomas.sitter@gmail.com>
2022-03-24 11:05:13 -0700

    Decouple home package from pantry_overview. Add controller to listen to tab changes

--- lib/pantry_overview/view/grocery_screen.dart
+++ lib/pantry_overview/view/grocery_screen.dart
@@ -4,17 +4,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pantry_repository/pantry_repository.dart';
 import 'package:the_pantry/pantry_overview/pantry_overview.dart';
 import 'package:the_pantry/edit_pantry_item/edit_pantry_item.dart';
-import 'package:the_pantry/home/home.dart';
 
 class GroceryScreen extends StatelessWidget {
   static const String id = 'grocery_screen';
+  final bool showGroceries;
 
-  const GroceryScreen({Key? key}) : super(key: key);
+  const GroceryScreen({Key? key, required this.showGroceries})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final homeState = context.watch<HomeCubit>().state;
-    bool showGroceries = homeState.tab == HomeTab.grocery;
+    print("Showing groceries: $showGroceries");
 
     return BlocProvider(
       create: (context) => PantryOverviewBloc(
