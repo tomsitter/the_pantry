003681585fbf1e525b6f793fb04fb033bbe28fae
Tom Sitter <thomas.sitter@gmail.com>
2022-04-11 14:28:37 -0700

    Add comment to describe password regex

--- packages/form_inputs/lib/src/password.dart
+++ packages/form_inputs/lib/src/password.dart
@@ -5,6 +5,7 @@ enum PasswordValidationError { invalid }
 class Password extends FormzInput<String, PasswordValidationError> {
   const Password.pure() : super.pure('');
   const Password.dirty([String value = '']) : super.dirty(value);
+  // password must have 8 characters, including a capital and a number
   static final _passwordRegExp =
       RegExp(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$');
 
