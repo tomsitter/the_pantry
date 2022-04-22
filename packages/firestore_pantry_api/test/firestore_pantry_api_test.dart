9ee8c7215e86464b6b0fde208fa6b524aeb825b3
Tom Sitter <thomas.sitter@gmail.com>
2022-04-21 19:17:31 -0700

    Finish adding tests for firestore_pantry_api

--- packages/firestore_pantry_api/test/firestore_pantry_api_test.dart
+++ packages/firestore_pantry_api/test/firestore_pantry_api_test.dart
@@ -79,6 +79,7 @@ void main() {
 
     group('updates items', () {
       late FirestorePantryApi firestorePantryApi;
+      late List<PantryItem> items;
 
       setUp(() async {
         final fakeFirestore = FakeFirebaseFirestore();
@@ -88,103 +89,149 @@ void main() {
             .set(pantryRaw);
         firestorePantryApi = FirestorePantryApi(instance: fakeFirestore);
         await firestorePantryApi.streamUserPantryItems(mockDocId);
-      });
-
-      // test('saves a new pantry item', () async {
-      //   final item = PantryItem(name: 'cucumber');
-      //   final int numItems = await firestorePantryApi.pantryItems.length;
-      //
-      //   firestorePantryApi.saveItem(mockDocId, item);
-      //   await expectLater(firestorePantryApi.pantryItems, emits([item]));
-      //   expect(firestorePantryApi.pantryItems.length, equals(numItems + 1));
-      // });
-
-      // test('updates an existing pantry item', () async {
-      //   final int numItems = await firestorePantryApi.pantryItems.length;
-      //
-      //   final item = pantryItems[0];
-      //   await firestorePantryApi.toggleChecked(
-      //       mockDocId, item, !item.isChecked);
-      //
-      //   firestorePantryApi.saveItem(mockDocId, item);
-      //   await expectLater(firestorePantryApi.pantryItems, emits([item]));
-      //   expect(firestorePantryApi.pantryItems.length, equals(numItems));
-      // });
-
-      test('toggleChecked', () async {
-        final List<PantryItem> items =
-            await firestorePantryApi.pantryItems.firstWhere(
+        items = await firestorePantryApi.pantryItems.firstWhere(
           (event) => event.length > 0,
           orElse: () => [],
         );
-        PantryItem newItem = items[0].copyWith(isChecked: !items[0].isChecked);
-        firestorePantryApi.toggleChecked(
-            mockDocId, items[0], !items[0].isChecked);
-        expect(
-          firestorePantryApi.pantryItems,
-          emitsThrough([newItem, items[1]]),
-        );
       });
 
-      test('toggleChecked throws PantryException if item not found', () async {
-        final List<PantryItem> items =
-            await firestorePantryApi.pantryItems.firstWhere(
-          (event) => event.length > 0,
-          orElse: () => [],
-        );
-        PantryItem newItem = PantryItem(name: 'new item');
-        expect(
-          firestorePantryApi.toggleChecked(mockDocId, newItem, true),
-          throwsA(isA<PantryException>()),
-        );
+      group('saveItem', () {
+        test('saves a new pantry item', () async {
+          final newItem = PantryItem(name: 'cucumber');
+          firestorePantryApi.saveItem(mockDocId, newItem);
+          firestorePantryApi.pantryItems.listen(
+            expectAsync1(
+              (value) => expect(
+                value,
+                equals(items..add(newItem)),
+              ),
+            ),
+          );
+        });
+
+        test('overwrites an existing pantry item', () async {
+          final newItem = items[0].copyWith(name: 'cucumber');
+          firestorePantryApi.saveItem(mockDocId, newItem);
+          firestorePantryApi.pantryItems.listen(
+            expectAsync1(
+              (value) => expect(
+                value,
+                equals([newItem, items[1]]),
+              ),
+            ),
+          );
+        });
       });
 
-      test('toggleInGroceries', () async {
-        final List<PantryItem> items =
-            await firestorePantryApi.pantryItems.firstWhere(
-          (event) => event.length > 0,
-          orElse: () => [],
-        );
-        PantryItem newItem =
-            items[0].copyWith(inGroceryList: !items[0].inGroceryList);
-        firestorePantryApi.toggleInGroceries(
-            mockDocId, items[0], !items[0].inGroceryList);
-        expect(
-          firestorePantryApi.pantryItems,
-          emitsThrough([newItem, items[1]]),
-        );
+      group('deleteItem', () {
+        test('deleteItem works', () {
+          firestorePantryApi.deleteItem(mockDocId, items[0]);
+          firestorePantryApi.pantryItems.listen(
+            expectAsync1(
+              (value) => expect(
+                value,
+                equals([items[1]]),
+              ),
+            ),
+          );
+        });
+
+        test('deleteItem throws PantryException if item not found', () async {
+          PantryItem newItem = PantryItem(name: 'new item');
+          expect(
+            firestorePantryApi.deleteItem(mockDocId, newItem),
+            throwsA(isA<PantryException>()),
+          );
+        });
       });
 
-      test('changeAmount', () async {
-        final List<PantryItem> items =
-            await firestorePantryApi.pantryItems.firstWhere(
-          (event) => event.length > 0,
-          orElse: () => [],
-        );
-        PantryItem newItem =
-            items[0].copyWith(amount: FoodAmount.fromString('empty'));
-        firestorePantryApi.changeAmount(
-            mockDocId, items[0], FoodAmount.fromString('empty'));
-        expect(
-          firestorePantryApi.pantryItems,
-          emitsThrough([newItem, items[1]]),
-        );
+      group('toggleChecked', () {
+        test('toggleChecked works', () async {
+          PantryItem newItem =
+              items[0].copyWith(isChecked: !items[0].isChecked);
+          firestorePantryApi.toggleChecked(
+              mockDocId, items[0], !items[0].isChecked);
+          expect(
+            firestorePantryApi.pantryItems,
+            emitsThrough([newItem, items[1]]),
+          );
+        });
+
+        test('toggleChecked throws PantryException if item not found',
+            () async {
+          PantryItem newItem = PantryItem(name: 'new item');
+          expect(
+            firestorePantryApi.toggleChecked(mockDocId, newItem, true),
+            throwsA(isA<PantryException>()),
+          );
+        });
       });
 
-      test('changeCategory', () async {
-        final List<PantryItem> items =
-            await firestorePantryApi.pantryItems.firstWhere(
-          (event) => event.length > 0,
-          orElse: () => [],
-        );
-        PantryItem newItem =
-            items[0].copyWith(category: FoodCategory.fromString('frozen'));
-        firestorePantryApi.changeCategory(
-            mockDocId, items[0], FoodCategory.fromString('frozen'));
-        expect(
-          firestorePantryApi.pantryItems,
-          emitsThrough([newItem, items[1]]),
-        );
+      group('toggleInGroceries', () {
+        test('toggleInGroceries works', () async {
+          PantryItem newItem =
+              items[0].copyWith(inGroceryList: !items[0].inGroceryList);
+          firestorePantryApi.toggleInGroceries(
+              mockDocId, items[0], !items[0].inGroceryList);
+          expect(
+            firestorePantryApi.pantryItems,
+            emitsThrough([newItem, items[1]]),
+          );
+        });
+
+        test('toggleInGroceries throws PantryException if item not found',
+            () async {
+          PantryItem newItem = PantryItem(name: 'new item');
+          expect(
+            firestorePantryApi.toggleInGroceries(mockDocId, newItem, true),
+            throwsA(isA<PantryException>()),
+          );
+        });
+      });
+
+      group('changeAmount', () {
+        test('changeAmount', () async {
+          PantryItem newItem =
+              items[0].copyWith(amount: FoodAmount.fromString('empty'));
+          firestorePantryApi.changeAmount(
+              mockDocId, items[0], FoodAmount.fromString('empty'));
+          expect(
+            firestorePantryApi.pantryItems,
+            emitsThrough([newItem, items[1]]),
+          );
+        });
+
+        test('changeAmount throws PantryException if item not found', () async {
+          PantryItem newItem = PantryItem(name: 'new item');
+          expect(
+            firestorePantryApi.changeAmount(
+                mockDocId, newItem, FoodAmount.full),
+            throwsA(isA<PantryException>()),
+          );
+        });
+      });
+
+      group('changeCategory', () {
+        test('changeCategory', () async {
+          PantryItem newItem =
+              items[0].copyWith(category: FoodCategory.fromString('frozen'));
+          firestorePantryApi.changeCategory(
+              mockDocId, items[0], FoodCategory.fromString('frozen'));
+          expect(
+            firestorePantryApi.pantryItems,
+            emitsThrough([newItem, items[1]]),
+          );
+        });
+
+        test('changeCategory throws PantryException if item not found',
+            () async {
+          PantryItem newItem = PantryItem(name: 'new item');
+          expect(
+            firestorePantryApi.changeCategory(
+                mockDocId, newItem, FoodCategory.uncategorized),
+            throwsA(isA<PantryException>()),
+          );
+        });
       });
     });
   });
